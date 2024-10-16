6f302087df5bbd70ed9b703f2ecd7591e2ec2231
Wasif Ali <91631681+wasif-dali@users.noreply.github.com>
2024-10-16 23:11:22 +0530

    add header in app.js file and create some folder with news letter and other home page footer

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import Footer from"./components/Footer/Footer";
 import Home from"./components/Home/Home";
 import Category from"./components/Category/Category";
 import SingleProduct from"./components/SingleProduct/SingleProduct";
-//import Newsletter from"./components/Newslatter/Newslatter";
-//import AppContext from"./utils/context";
+import Newsletter from"./components/Newsletter/Newsletter";
+import AppContext from"./utils/context";
 function App() {
     return (
         <BrowserRouter>
+       <AppContext>
+        <Header/>
          <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/category/:id" element={<Category />}/>
             <Route path="/product/:id" element={<SingleProduct />}/>
-
-        </Routes>
+       </Routes>
+       <Newsletter />
+       <Footer /> 
+       </AppContext>
     </BrowserRouter>
     );
 }
